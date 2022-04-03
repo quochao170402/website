2ca9afb3d61434937b6ecea37e7236db2f856875
unknown <quochao170402@gmail.com>
2022-04-03 21:10:03 +0700

    first commit

---
+++ src/test/java/com/quochao/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quochao.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
