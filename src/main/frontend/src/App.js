02be61bc2177578c76d812d5e89d3e055682b7ba
unknown <quochao170402@gmail.com>
2022-04-03 23:48:27 +0700

    test cho vui

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// Test thử cho vui thôi nha
+const DemoCode = () => {
+
+  const fecthData = () => {
+    axios.get("http://localhost:8080").then(res => {
+      console.log(res);
+    });
+  }
+
+  useEffect(() => {
+    fecthData();
+  })
+
+  return <h1>Hello</h1>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Hello React with Spring Boot
-        </a>
-      </header>
+      <DemoCode />
     </div>
   );
 }
