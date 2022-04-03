deaa352dbfae2ebfcbd779366904d1d0cc9541bb
unknown <quochao170402@gmail.com>
2022-04-04 06:58:26 +0700

    commit

--- src/main/java/com/quochao/website/controller/HomeController.java
+++ src/main/java/com/quochao/website/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "")
-@CrossOrigin
+@CrossOrigin("*")
 public class HomeController {
 
     @GetMapping
