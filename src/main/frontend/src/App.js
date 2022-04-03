520451c7e2e393db59292b2bdd8697c9f7a6eabc
unknown <quochao170402@gmail.com>
2022-04-03 22:35:31 +0700

    test commit

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Hello React with Spring Boot
         </a>
       </header>
     </div>
