6bd2b0240816c85efe5bcc5c82b05ca330ecee24
unknown <quochao170402@gmail.com>
2022-04-04 00:00:16 +0700

    deploy frontend to heroku

--- src/main/java/com/quochao/website/controller/HomeController.java
+++ src/main/java/com/quochao/website/controller/HomeController.java
@@ -1,11 +1,13 @@
 package com.quochao.website.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "")
+@CrossOrigin("*")
 public class HomeController {
 
     @GetMapping
