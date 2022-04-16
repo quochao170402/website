a12fdadcfa3fc7420966c42f2f2ad2895cfdde0f
local <quochao170402@gmail.com>
2022-04-16 15:58:56 +0700

    make it better

--- src/main/java/com/quochao/website/repository/TagRepository.java
+++
@@ -1,7 +0,0 @@
-package com.quochao.website.repository;
-
-import com.quochao.website.entity.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag,Long> {
-}
