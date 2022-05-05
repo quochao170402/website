a9632a65d2b8bf4c8c9303aad3d5cdd259032d96
local <quochao170402@gmail.com>
2022-05-05 08:50:21 +0700

    update

--- src/main/java/com/quochao/website/controller/RegisterController.java
+++ src/main/java/com/quochao/website/controller/RegisterController.java
@@ -4,10 +4,7 @@ import com.quochao.website.entity.User;
 import com.quochao.website.service.UserService;
 import lombok.Data;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/register")
@@ -16,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RegisterController {
     private final UserService userService;
 
+    @PostMapping
     public ResponseEntity<?> register(@ModelAttribute User user){
         return ResponseEntity.ok(userService.save(user));
     }
